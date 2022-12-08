63af2d49df44788ed382036ee31aa8c21eceb017
UP2013168 <91673755+UP2013168@users.noreply.github.com>
2022-12-08 23:52:53 +0000

    finally done part 1

--- day-7/part-1.py
+++ day-7/part-1.py
@@ -1,55 +1,37 @@
 f = open('day-7/input.txt', 'r')
 
-class DirectoryTree:
-    def __init__(self, name):
-        self.name = name
-        self.children = []
-        self.parent = None
-
-    def add_child(self, child):
-        self.children.append(child)
-        child.parent = self
-
-    def get_parent(self):
-        return self.parent
-
-    def get_children(self):
-        return self.children
-
-    def get_name(self):
-        return self.name
-
-    def get_all_children(self):
-        children = self.get_children()
-        for child in children:
-            children += child.get_all_children()
-        return children
-
-    def get_all_parents(self):
-        parent = self.get_parent()
-        if parent is None:
-            return []
-        return [parent] + parent.get_all_parents()
-
-    def __repr__(self):
-        return self.name
-
-directory = DirectoryTree('/')
-current_node = directory
+dirs = {}
+subdirs = {}
 path = "/"
 
-for line in f:
-  if '$ cd ..' in line:
-      current_node = directory.get_parent()
-      path = "".join(path.split('/')[:-2]) + "/"
-  elif '$ cd /' in line:
-      current_node = directory
-      path = "/"
-  elif '$ cd' in line:
-      path += line.split('$ cd ')[1].strip() + "/"
-      new_directory = DirectoryTree(path)
-      current_node.add_child(new_directory)
-      current_node = current_node.get_children()[len(current_node.get_children()) - 1]
-      
 
-print(directory.get_all_children())
+def count_sizes(path):
+  result = dirs[path]
+  for subdir in subdirs[path]:
+    if subdir in dirs:
+      result += count_sizes(subdir)
+  return result
+
+for line in f:
+  if '$' in line:
+    if '$ cd ..' in line:
+        path = "/".join(path.split('/')[:-2]) + "/"
+    elif '$ cd /' in line:
+        path = "/"
+    elif '$ cd' in line:
+        path += line.split('$ cd ')[1].strip() + "/"
+    if path not in dirs:
+        dirs[path] = 0
+        subdirs[path] = []
+  else:
+    if 'dir' in line:
+        subdirs[path].append(path + line.split('dir ')[1].strip() + "/")
+    else:
+        dirs[path] += int(line.split(' ')[0].strip())
+
+count = 0   
+for dir in dirs:
+  if count_sizes(dir) <= 100000:
+    count += count_sizes(dir)
+
+print(count)
